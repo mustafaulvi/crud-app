53cf3a74d0136954f7ca00b8e74b16441c01dbfe
mustafaulvi <ulvimustafa@gmail.com>
2023-07-23 23:21:41 +0200

    Variable defined for server connection information

--- src/app/services/person.service.ts
+++ src/app/services/person.service.ts
@@ -2,6 +2,11 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
+//const url = "http://localhost:3000/person";
+const url = "http://localhost:8081/person";
+
+
+
 @Injectable({
   providedIn: 'root'
 })
@@ -10,15 +15,15 @@ export class PersonService {
   constructor(private _http: HttpClient ) { }
 
   addPerson(date:any): Observable<any>{
-    return this._http.post('http://localhost:3000/person',date );
+    return this._http.post(url,date );
   }
   updatePerson(id:number, date:any): Observable<any>{
-    return this._http.put('http://localhost:3000/person/'+id,date );
+    return this._http.put(url+'/'+id,date );
   }
   getPersonList (): Observable<any>{
-    return this._http.get('http://localhost:3000/person');
+    return this._http.get(url);
   }
   deletePersonList (id: number): Observable<any>{
-    return this._http.delete ('http://localhost:3000/person/'+id);
+    return this._http.delete (url+'/'+id);
   }
 }
